3858f840cdce142e284932e528f7ff82d441e18f
Олег Адамов <OlegAdamov@mac-mini.local>
2015-09-01 18:47:07 +0300

    - end working controller

--- AppPermissions-Example/ViewController.swift
+++ AppPermissions-Example/ViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
-class PermissionSwitch : UISwitch {
-    var type: PermissionType?
+extension Array {
+    mutating func remove <U: Equatable> (object: U) {
+        for i in stride(from: self.count-1, through: 0, by: -1) {
+            if let element = self[i] as? U {
+                if element == object {
+                    self.removeAtIndex(i)
+                }
+            }
+        }
+    }
 }
+
+class PermissionCell: UITableViewCell {
+    
+    @IBOutlet weak var permissionLabel: UILabel!
+    @IBOutlet weak var permissionSwitch: UISwitch!
+    
+}
+
 
 class ViewController: UITableViewController {
 
+    
+    let typesData: [PermissionType] = [.AssetLibrary, .Camera, .Calendars, .Contacts, .CoreLocationAlways, .CoreLocationInUse, .Microphone, .Notifications, .Reminders]
+    var needTypes = [PermissionType]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +43,12 @@ class ViewController: UITableViewController {
     
     
     @objc private func doneButtonPressed() {
+        
+        
+        AppPermissionsViewController.controller(self).present(needTypes, completion: { (success) -> Void in
+            self.navigationItem.title = success ?  "Success" : "Failure"
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,11 +57,42 @@ class ViewController: UITableViewController {
     }
     
     
+    // MARK: - UITableViewDataSource
     
     override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return CGFloat.min
     }
-
-
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return typesData.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PermissionCell") as! PermissionCell
+        let permissionType = typesData[indexPath.row]
+        cell.permissionLabel.text = permissionType.rawValue
+        cell.permissionSwitch.tag = indexPath.row
+        cell.permissionSwitch.addTarget(self, action: "switchValueChanged:", forControlEvents: UIControlEvents.ValueChanged)
+        if contains(needTypes, permissionType) {
+            cell.permissionSwitch.setOn(true, animated: false)
+        }
+        
+        return cell
+    }
+    
+    
+    @objc private func switchValueChanged(sender: UISwitch) {
+        let type = typesData[sender.tag]
+        let isContained = contains(needTypes, type)
+        if sender.on  && !isContained {
+            needTypes.append(type)
+        } else if !sender.on && isContained {
+            needTypes.remove(type)
+        }
+        
+    }
+    
 }
 
